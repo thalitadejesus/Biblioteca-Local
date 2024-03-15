f3834e32767695e9f2b4547335be67eb39e18a16
thalitadejesus <128142629+thalitadejesus@users.noreply.github.com>
2024-03-15 10:04:53 -0300

    Add files via upload

--- Prova/locallibrary/catalog/admin.py
+++ Prova/locallibrary/catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
